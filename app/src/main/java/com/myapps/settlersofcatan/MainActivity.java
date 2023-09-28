6683d506f393a6a6c24f7889c5dfc0f2d8568e04
Daniel Greer <danielgreer19@gmail.com>
2023-09-27 18:29:12 -0600

    9 27 23 6:30pm

--- app/src/main/java/com/myapps/settlersofcatan/MainActivity.java
+++ app/src/main/java/com/myapps/settlersofcatan/MainActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
@@ -20,7 +21,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     TextView road_0, road_1, road_2, road_3, road_4, road_5, road_6, road_7, road_8, road_9, road_10, road_11, road_12, road_13, road_14, road_15, road_16, road_17, road_18, road_19, road_20, road_21, road_22, road_23, road_24, road_25, road_26, road_27, road_28, road_29, road_30, road_31, road_32, road_33, road_34, road_35, road_36, road_37, road_38, road_39, road_40, road_41, road_42, road_43, road_44, road_45, road_46, road_47, road_48, road_49, road_50, road_51, road_52, road_53, road_54, road_55, road_56, road_57, road_58, road_59, road_60, road_61, road_62, road_63, road_64, road_65, road_66, road_67, road_68, road_69, road_70, road_71, road_72, road_73, road_74, road_75, road_76, road_77, road_78, road_79, road_80, road_81, road_82, road_83, road_84, road_85, road_86, road_87, road_88, road_89, road_90, road_91, road_92, road_93, road_94, road_95, road_96, road_97, road_98, road_99, road_100, road_101, road_102, road_103, road_104, road_105, road_106, road_107, road_108, road_109, road_110, road_111, road_112;
 
-    TextView settlement_0, settlement_1, settlement_2, settlement_3, settlement_4, settlement_5, settlement_6, settlement_7, settlement_8, settlement_9, settlement_10, settlement_11, settlement_12, settlement_13, settlement_14, settlement_15, settlement_16, settlement_17, settlement_18, settlement_19, settlement_20, settlement_21, settlement_22, settlement_23, settlement_24, settlement_25, settlement_26, settlement_27, settlement_28, settlement_29, settlement_30, settlement_31, settlement_32, settlement_33, settlement_34, settlement_35, settlement_36, settlement_37, settlement_38, settlement_39, settlement_40, settlement_41, settlement_42, settlement_43, settlement_44, settlement_45, settlement_46, settlement_47, settlement_48, settlement_49, settlement_50, settlement_51, settlement_52, settlement_53, settlement_54, settlement_55, settlement_56, settlement_57, settlement_58, settlement_59, settlement_60, settlement_61, settlement_62, settlement_63, settlement_64, settlement_65, settlement_66, settlement_67, settlement_68, settlement_69, settlement_70, settlement_71, settlement_72, settlement_73, settlement_74, settlement_75, settlement_76, settlement_77, settlement_78, settlement_79, settlement_80, settlement_81, settlement_82, settlement_83, settlement_84, settlement_85, settlement_86, settlement_87, settlement_88, settlement_89, settlement_90, settlement_91, settlement_92, settlement_93, settlement_94, settlement_95, settlement_96, settlement_97, settlement_98, settlement_99, settlement_100, settlement_101, settlement_102, settlement_103, settlement_104, settlement_105, settlement_106, settlement_107, settlement_108, settlement_109, settlement_110;
+    TextView settlement_0, settlement_1, settlement_2, settlement_3, settlement_4, settlement_5, settlement_6, settlement_7, settlement_8, settlement_9, settlement_10, settlement_11, settlement_12, settlement_13, settlement_14, settlement_15, settlement_16, settlement_17, settlement_18, settlement_19, settlement_20, settlement_21, settlement_22, settlement_23, settlement_24, settlement_25, settlement_26, settlement_27, settlement_28, settlement_29, settlement_30, settlement_31, settlement_32, settlement_33, settlement_34, settlement_35, settlement_36, settlement_37, settlement_38, settlement_39, settlement_40, settlement_41, settlement_42, settlement_43, settlement_44, settlement_45, settlement_46, settlement_47, settlement_48, settlement_49, settlement_50, settlement_51, settlement_52, settlement_53, settlement_54, settlement_55, settlement_56, settlement_57, settlement_58, settlement_59, settlement_60, settlement_61, settlement_62, settlement_63, settlement_64, settlement_65, settlement_66, settlement_67, settlement_68, settlement_69, settlement_70, settlement_71, settlement_72, settlement_73, settlement_74, settlement_75, settlement_76, settlement_77, settlement_78, settlement_79, settlement_80, settlement_81, settlement_82, settlement_83, settlement_84, settlement_85, settlement_86, settlement_87, settlement_88, settlement_89, settlement_90, settlement_91, settlement_92, settlement_93, settlement_94, settlement_95, settlement_96, settlement_97, settlement_98, settlement_99, settlement_100, settlement_101, settlement_102, settlement_103, settlement_104, settlement_105, settlement_106, settlement_107, settlement_108, settlement_109, settlement_110, settlement_111, settlement_112, settlement_113, settlement_114, settlement_115, settlement_116, settlement_117, settlement_118, settlement_119, settlement_120, settlement_121, settlement_122, settlement_123;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -301,6 +302,253 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
 
 
+        settlement_0 = findViewById(R.id.settlement_0);
+        settlement_0.setOnClickListener(this);
+
+        settlement_1 = findViewById(R.id.settlement_1);
+        settlement_1.setOnClickListener(this);
+
+        settlement_2 = findViewById(R.id.settlement_2);
+        settlement_2.setOnClickListener(this);
+
+        settlement_3 = findViewById(R.id.settlement_3);
+        settlement_3.setOnClickListener(this);
+
+        settlement_4 = findViewById(R.id.settlement_4);
+        settlement_4.setOnClickListener(this);
+
+        settlement_5 = findViewById(R.id.settlement_5);
+        settlement_5.setOnClickListener(this);
+
+        settlement_6 = findViewById(R.id.settlement_6);
+        settlement_6.setOnClickListener(this);
+
+        settlement_7 = findViewById(R.id.settlement_7);
+        settlement_7.setOnClickListener(this);
+
+        settlement_8 = findViewById(R.id.settlement_8);
+        settlement_8.setOnClickListener(this);
+
+        settlement_9 = findViewById(R.id.settlement_9);
+        settlement_9.setOnClickListener(this);
+
+        settlement_10 = findViewById(R.id.settlement_10);
+        settlement_10.setOnClickListener(this);
+
+        settlement_11 = findViewById(R.id.settlement_11);
+        settlement_11.setOnClickListener(this);
+
+        settlement_12 = findViewById(R.id.settlement_12);
+        settlement_12.setOnClickListener(this);
+
+        settlement_13 = findViewById(R.id.settlement_13);
+        settlement_13.setOnClickListener(this);
+
+        settlement_14 = findViewById(R.id.settlement_14);
+        settlement_14.setOnClickListener(this);
+
+        settlement_15 = findViewById(R.id.settlement_15);
+        settlement_15.setOnClickListener(this);
+
+        settlement_17 = findViewById(R.id.settlement_17);
+        settlement_17.setOnClickListener(this);
+
+        settlement_19 = findViewById(R.id.settlement_19);
+        settlement_19.setOnClickListener(this);
+
+        settlement_20 = findViewById(R.id.settlement_20);
+        settlement_20.setOnClickListener(this);
+
+        settlement_21 = findViewById(R.id.settlement_21);
+        settlement_21.setOnClickListener(this);
+
+        settlement_22 = findViewById(R.id.settlement_22);
+        settlement_22.setOnClickListener(this);
+
+        settlement_23 = findViewById(R.id.settlement_23);
+        settlement_23.setOnClickListener(this);
+
+        settlement_39 = findViewById(R.id.settlement_39);
+        settlement_39.setOnClickListener(this);
+
+        settlement_40 = findViewById(R.id.settlement_40);
+        settlement_40.setOnClickListener(this);
+
+        settlement_41 = findViewById(R.id.settlement_41);
+        settlement_41.setOnClickListener(this);
+
+        settlement_42 = findViewById(R.id.settlement_42);
+        settlement_42.setOnClickListener(this);
+
+        settlement_45 = findViewById(R.id.settlement_45);
+        settlement_45.setOnClickListener(this);
+
+        settlement_46 = findViewById(R.id.settlement_46);
+        settlement_46.setOnClickListener(this);
+
+        settlement_47 = findViewById(R.id.settlement_47);
+        settlement_47.setOnClickListener(this);
+
+        settlement_48 = findViewById(R.id.settlement_48);
+        settlement_48.setOnClickListener(this);
+
+        settlement_51 = findViewById(R.id.settlement_51);
+        settlement_51.setOnClickListener(this);
+
+        settlement_53 = findViewById(R.id.settlement_53);
+        settlement_53.setOnClickListener(this);
+
+        settlement_54 = findViewById(R.id.settlement_54);
+        settlement_54.setOnClickListener(this);
+
+        settlement_55 = findViewById(R.id.settlement_55);
+        settlement_55.setOnClickListener(this);
+
+        settlement_57 = findViewById(R.id.settlement_57);
+        settlement_57.setOnClickListener(this);
+
+        settlement_59 = findViewById(R.id.settlement_59);
+        settlement_59.setOnClickListener(this);
+
+        settlement_60 = findViewById(R.id.settlement_60);
+        settlement_60.setOnClickListener(this);
+
+        settlement_61 = findViewById(R.id.settlement_61);
+        settlement_61.setOnClickListener(this);
+
+        settlement_62 = findViewById(R.id.settlement_62);
+        settlement_62.setOnClickListener(this);
+
+        settlement_63 = findViewById(R.id.settlement_63);
+        settlement_63.setOnClickListener(this);
+
+        settlement_65 = findViewById(R.id.settlement_65);
+        settlement_65.setOnClickListener(this);
+
+        settlement_66 = findViewById(R.id.settlement_66);
+        settlement_66.setOnClickListener(this);
+
+        settlement_67 = findViewById(R.id.settlement_67);
+        settlement_67.setOnClickListener(this);
+
+        settlement_68 = findViewById(R.id.settlement_68);
+        settlement_68.setOnClickListener(this);
+
+        settlement_69 = findViewById(R.id.settlement_69);
+        settlement_69.setOnClickListener(this);
+
+        settlement_72 = findViewById(R.id.settlement_72);
+        settlement_72.setOnClickListener(this);
+
+        settlement_73 = findViewById(R.id.settlement_73);
+        settlement_73.setOnClickListener(this);
+
+        settlement_74 = findViewById(R.id.settlement_74);
+        settlement_74.setOnClickListener(this);
+
+        settlement_75 = findViewById(R.id.settlement_75);
+        settlement_75.setOnClickListener(this);
+
+        settlement_76 = findViewById(R.id.settlement_76);
+        settlement_76.setOnClickListener(this);
+
+        settlement_77 = findViewById(R.id.settlement_77);
+        settlement_77.setOnClickListener(this);
+
+        settlement_80 = findViewById(R.id.settlement_80);
+        settlement_80.setOnClickListener(this);
+
+        settlement_82 = findViewById(R.id.settlement_82);
+        settlement_82.setOnClickListener(this);
+
+        settlement_84 = findViewById(R.id.settlement_84);
+        settlement_84.setOnClickListener(this);
+
+        settlement_85 = findViewById(R.id.settlement_85);
+        settlement_85.setOnClickListener(this);
+
+        settlement_88 = findViewById(R.id.settlement_88);
+        settlement_88.setOnClickListener(this);
+
+        settlement_89 = findViewById(R.id.settlement_89);
+        settlement_89.setOnClickListener(this);
+
+        settlement_95 = findViewById(R.id.settlement_95);
+        settlement_95.setOnClickListener(this);
+
+        settlement_99 = findViewById(R.id.settlement_99);
+        settlement_99.setOnClickListener(this);
+
+        settlement_100 = findViewById(R.id.settlement_100);
+        settlement_100.setOnClickListener(this);
+
+        settlement_101 = findViewById(R.id.settlement_101);
+        settlement_101.setOnClickListener(this);
+
+        settlement_104 = findViewById(R.id.settlement_104);
+        settlement_104.setOnClickListener(this);
+
+        settlement_105 = findViewById(R.id.settlement_105);
+        settlement_105.setOnClickListener(this);
+
+        settlement_106 = findViewById(R.id.settlement_106);
+        settlement_106.setOnClickListener(this);
+
+        settlement_107 = findViewById(R.id.settlement_107);
+        settlement_107.setOnClickListener(this);
+
+        settlement_108 = findViewById(R.id.settlement_108);
+        settlement_108.setOnClickListener(this);
+
+        settlement_109 = findViewById(R.id.settlement_109);
+        settlement_109.setOnClickListener(this);
+
+        settlement_110 = findViewById(R.id.settlement_110);
+        settlement_110.setOnClickListener(this);
+
+        settlement_111 = findViewById(R.id.settlement_111);
+        settlement_111.setOnClickListener(this);
+
+        settlement_112 = findViewById(R.id.settlement_112);
+        settlement_112.setOnClickListener(this);
+
+        settlement_113 = findViewById(R.id.settlement_113);
+        settlement_113.setOnClickListener(this);
+
+        settlement_114 = findViewById(R.id.settlement_114);
+        settlement_114.setOnClickListener(this);
+
+        settlement_115 = findViewById(R.id.settlement_115);
+        settlement_115.setOnClickListener(this);
+
+        settlement_116 = findViewById(R.id.settlement_116);
+        settlement_116.setOnClickListener(this);
+
+        settlement_117 = findViewById(R.id.settlement_117);
+        settlement_117.setOnClickListener(this);
+
+        settlement_118 = findViewById(R.id.settlement_118);
+        settlement_118.setOnClickListener(this);
+
+        settlement_119 = findViewById(R.id.settlement_119);
+        settlement_119.setOnClickListener(this);
+
+        settlement_120 = findViewById(R.id.settlement_120);
+        settlement_120.setOnClickListener(this);
+
+        settlement_121 = findViewById(R.id.settlement_121);
+        settlement_121.setOnClickListener(this);
+
+        settlement_122 = findViewById(R.id.settlement_122);
+        settlement_122.setOnClickListener(this);
+
+        settlement_123 = findViewById(R.id.settlement_123);
+        settlement_123.setOnClickListener(this);
+
+
+
+
+
 
 
 
@@ -434,7 +682,17 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     @Override
     public void onClick(View v) {
+        //gets the type of view whether road or settlement
+        char viewType = v.getResources().getResourceName(v.getId()).charAt(30);
+
+        if (viewType == 's') {
+            v.setBackground(ResourcesCompat.getDrawable(getResources(), R.drawable.blue_settlement, getTheme()));
+        }
+
+        if (viewType == 'r') {
+            v.setBackgroundColor(ResourcesCompat.getColor(getResources(), R.color.blue, getTheme()));
+        }
+
         v.setAlpha(1);
-        v.setBackgroundColor(ResourcesCompat.getColor(getResources(), R.color.blue, getTheme()));
     }
 }
